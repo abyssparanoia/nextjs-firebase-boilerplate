7de9584f831faee214c3d51e40df55e3178c9e8a
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-06-30 16:38:07 +0900

    fix: type def

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -3,3 +3,15 @@ import * as admin from 'firebase-admin'
 export default admin.initializeApp({
   credential: admin.credential.cert(require('../firebaseAdminKey.json'))
 })
+
+declare global {
+  namespace Express {
+    interface Request {
+      firebaseServer: admin.app.App
+    }
+    interface Session {
+      firebaseUser: admin.auth.DecodedIdToken
+      firebaseToken: string
+    }
+  }
+}
