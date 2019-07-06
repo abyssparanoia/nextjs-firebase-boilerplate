c962d3dbd94f19b1b9f93d9307b64e1f3a5f2d9e
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-07-06 14:25:16 +0900

    feat: add axios instance

---
+++ modules/httpClient.ts
@@ -0,0 +1,42 @@
+import { default as axios, AxiosResponse } from 'axios'
+
+export default class AxiosClient {
+  public basedUrl: string
+  public token?: string
+
+  private readonly axiosInstance = axios.create({
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: this.token ? `Bearer ${this.token}` : undefined
+    }
+  })
+
+  public constructor({ url, token }: { url: string; token?: string }) {
+    this.basedUrl = url
+    this.token = token
+  }
+
+  public async get(params: object = {}, id?: string): Promise<AxiosResponse> {
+    const url = id === undefined ? this.basedUrl : `${this.basedUrl}/${id}`
+    return this.axiosInstance.get(url, { params })
+  }
+
+  public async post(params: object): Promise<AxiosResponse> {
+    return this.axiosInstance.post(this.basedUrl, params)
+  }
+
+  public async put(params: object, id: string): Promise<AxiosResponse> {
+    const url = `${this.basedUrl}/${id}`
+    return this.axiosInstance.put(url, params)
+  }
+
+  public async patch(params: object, id: string): Promise<AxiosResponse> {
+    const url = `${this.basedUrl}/${id}`
+    return this.axiosInstance.patch(url, params)
+  }
+
+  public async delete(id: string): Promise<AxiosResponse> {
+    const url = `${this.basedUrl}/${id}`
+    return this.axiosInstance.delete(url)
+  }
+}
