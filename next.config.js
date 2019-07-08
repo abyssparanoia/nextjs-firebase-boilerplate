dccda8096696896ff8e3023772dc5ba9c10feac7
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-07-09 02:10:19 +0900

    feat: add webpack and dotenv

--- next.config.js
+++ next.config.js
@@ -1,2 +1,27 @@
+require('dotenv').config()
+
 const withTypescript = require('@zeit/next-typescript')
-module.exports = withTypescript()
+const path = require('path')
+const Dotenv = require('dotenv-webpack')
+
+module.exports = withTypescript({
+  webpack: config => {
+    config.plugins = config.plugins || []
+
+    config.plugins = [
+      ...config.plugins,
+
+      new Dotenv({
+        path: path.join(__dirname, '.env'),
+        systemvars: true
+      })
+    ]
+
+    config.resolve.alias['modules'] = path.join(__dirname, 'modules')
+    config.resolve.alias['pages'] = path.join(__dirname, 'pages')
+    config.resolve.alias['contexts'] = path.join(__dirname, 'contexts')
+    config.resolve.alias['components'] = path.join(__dirname, 'components')
+
+    return config
+  }
+})
