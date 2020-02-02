ee9048e7671fdb5b32b5db57d76538ffeebc4805
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-02-03 03:32:33 +0900

    feat: added config for replace preact

--- next.config.js
+++ next.config.js
@@ -21,6 +21,24 @@ module.exports = {
     config.resolve.alias['pages'] = path.join(__dirname, '/src/pages')
     config.resolve.alias['components'] = path.join(__dirname, '/src/components')
 
+    const splitChunks = config.optimization && config.optimization.splitChunks
+    if (splitChunks) {
+      const cacheGroups = splitChunks.cacheGroups
+      const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/
+      if (cacheGroups.framework) {
+        cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
+          test: preactModules
+        })
+        cacheGroups.commons.name = 'framework'
+      } else {
+        cacheGroups.preact = {
+          name: 'commons',
+          chunks: 'all',
+          test: preactModules
+        }
+      }
+    }
+
     return config
   },
   distDir: 'dist/src/.next'
