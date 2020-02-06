32bcf5d785cf25467d4bc11e2ff86547b05b63d4
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-02-07 00:40:50 +0900

    feat: use interface in nestjs

--- packages/backend/src/app.controller.ts
+++ packages/backend/src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Post, Body } from '@nestjs/common'
 import { AppService } from './app.service'
+import { SignInRequestDto } from './app.dto'
 
 @Controller()
 export class AppController {
@@ -8,5 +9,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello()
+  }
+
+  @Post()
+  async signIn(@Body() signInRequestDto: SignInRequestDto) {
+    // eslint-disable-next-line no-console
+    console.log(signInRequestDto)
+    return {
+      accessToken: 'accessToken',
+      refreshToken: 'refreshToken'
+    }
   }
 }
