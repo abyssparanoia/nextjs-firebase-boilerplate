dd72c71388a76aee5c25c7ed0e05f9b7d0594be1
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-02-13 04:39:49 +0900

    fix: remove error in reducer

--- packages/web/src/modules/reducer.ts
+++ packages/web/src/modules/reducer.ts
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux'
-import { State as ErrorState, reducer as errorReducer } from './error'
 import { State as TableState, reducer as tableReducer } from './table'
 import { State as AuthState, reducer as authReducer } from './auth'
 import { State as FeedbackState, reducer as feedbackReducer } from './feedback'
 
 export interface ReduxStore {
-  error: ErrorState
   table: TableState
   auth: AuthState
   feedback: FeedbackState
@@ -13,7 +11,6 @@ export interface ReduxStore {
 
 export const createRootReducer = () =>
   combineReducers({
-    error: errorReducer,
     table: tableReducer,
     auth: authReducer,
     feedback: feedbackReducer
