95d038235a63d290f24a4dca04fab82a7ad2921f
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-02-13 03:49:28 +0900

    feat: added state in reducer

--- packages/web/src/modules/reducer.ts
+++ packages/web/src/modules/reducer.ts
@@ -2,16 +2,19 @@ import { combineReducers } from 'redux'
 import { State as ErrorState, reducer as errorReducer } from './error'
 import { State as TableState, reducer as tableReducer } from './table'
 import { State as AuthState, reducer as authReducer } from './auth'
+import { State as FeedbackState, reducer as feedbackReducer } from './feedback'
 
 export interface ReduxStore {
   error: ErrorState
   table: TableState
   auth: AuthState
+  feedback: FeedbackState
 }
 
 export const createRootReducer = () =>
   combineReducers({
     error: errorReducer,
     table: tableReducer,
-    auth: authReducer
+    auth: authReducer,
+    feedback: feedbackReducer
   })
