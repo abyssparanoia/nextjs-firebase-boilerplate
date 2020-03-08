a0c4ebf08994219ba7521190b5d98d557367923b
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-03-08 14:05:22 +0900

    feat: added authentication component

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -4,7 +4,7 @@ import { IonApp, IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, Io
 import { IonReactRouter } from '@ionic/react-router'
 import { ellipse, square, triangle } from 'ionicons/icons'
 import { ConnectedRouter } from 'connected-react-router'
-import { withRouter } from 'react-router'
+import { useHistory } from 'react-router'
 import { Provider } from 'react-redux'
 import { makeStore } from './store'
 
@@ -30,38 +30,47 @@ import '@ionic/react/css/display.css'
 
 /* Theme variables */
 import './theme/variables.css'
+import SignIn from './pages/SignIn'
+import { Authentication } from './components/global/Authentication'
+import { Authorization } from './components/global/Authorization'
 
-const RouterApp = withRouter(({ history }) => {
+const RouterApp = () => {
+  const history = useHistory()
   const store = makeStore(history)
   return (
     <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <IonTabs>
-          <IonRouterOutlet>
-            <Route path="/tab1" component={Tab1} exact={true} />
-            <Route path="/tab2" component={Tab2} exact={true} />
-            <Route path="/tab3" component={Tab3} />
-            <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
-          </IonRouterOutlet>
-          <IonTabBar slot="bottom">
-            <IonTabButton tab="tab1" href="/tab1">
-              <IonIcon icon={triangle} />
-              <IonLabel>Tab 1</IonLabel>
-            </IonTabButton>
-            <IonTabButton tab="tab2" href="/tab2">
-              <IonIcon icon={ellipse} />
-              <IonLabel>Tab 2</IonLabel>
-            </IonTabButton>
-            <IonTabButton tab="tab3" href="/tab3">
-              <IonIcon icon={square} />
-              <IonLabel>Tab 3</IonLabel>
-            </IonTabButton>
-          </IonTabBar>
-        </IonTabs>
-      </ConnectedRouter>
+      <Authentication>
+        <ConnectedRouter history={history}>
+          <Authorization>
+            <IonTabs>
+              <IonRouterOutlet>
+                <Route path="/tab1" component={Tab1} exact={true} />
+                <Route path="/tab2" component={Tab2} exact={true} />
+                <Route path="/tab3" component={Tab3} />
+                <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
+              </IonRouterOutlet>
+              <IonTabBar slot="bottom">
+                <IonTabButton tab="tab1" href="/tab1">
+                  <IonIcon icon={triangle} />
+                  <IonLabel>Tab 1</IonLabel>
+                </IonTabButton>
+                <IonTabButton tab="tab2" href="/tab2">
+                  <IonIcon icon={ellipse} />
+                  <IonLabel>Tab 2</IonLabel>
+                </IonTabButton>
+                <IonTabButton tab="tab3" href="/tab3">
+                  <IonIcon icon={square} />
+                  <IonLabel>Tab 3</IonLabel>
+                </IonTabButton>
+              </IonTabBar>
+            </IonTabs>
+          </Authorization>
+          <Route path="/sign_in" component={SignIn} exact={true} />
+        </ConnectedRouter>
+      </Authentication>
     </Provider>
   )
-})
+}
 
 const App: React.FC = () => (
   <IonApp>
