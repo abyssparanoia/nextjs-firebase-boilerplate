687fd2715b44ad8ad010d5f4b10c722c49e01a91
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-03-15 23:49:49 +0900

    refactor: remove session

--- packages/web/src/factory.ts
+++ packages/web/src/factory.ts
@@ -1,26 +1,13 @@
 import express, { Request, Response } from 'express'
 import next from 'next'
 import { auth, db } from './firebase/admin'
-import { Credential } from './firebase/interface'
-import session, { SessionOptions } from 'express-session'
 import * as bodyParser from 'body-parser'
 import cors from 'cors'
-import { FireSessionStore } from './fire-sesion-store'
 
 const dev = process.env.NODE_ENV !== 'production'
 
 const app = next({ dir: '.', dev })
 const handle = app.getRequestHandler()
-
-const sessionOptions: SessionOptions = {
-  name: '__session',
-  secret: 'secretString',
-  store: new FireSessionStore({ db }),
-  resave: false,
-  saveUninitialized: false,
-  rolling: true,
-  cookie: { maxAge: 604800000 } // week
-}
 
 export const appFactory = async () => {
   await app.prepare()
@@ -28,27 +15,11 @@ export const appFactory = async () => {
 
   server.use(cors())
   server.use(bodyParser.json())
-  server.use(session(sessionOptions))
 
   server.use((req: Request, _: Response, next: Function) => {
     req.firebaseAuth = auth
     req.firebaseStore = db
     next()
-  })
-
-  server.post('/api/session', async (req: Request, res: Response) => {
-    if (!req.body) return res.sendStatus(400)
-
-    const { accessToken, refreshToken, uid } = req.body as Credential
-
-    req.session!.credential = { accessToken, refreshToken, uid }
-
-    return res.sendStatus(200)
-  })
-
-  server.delete('/api/session', async (req: Request, res: Response) => {
-    await req.session!.destroy(() => {})
-    return res.sendStatus(200)
   })
 
   // nextjs routing
