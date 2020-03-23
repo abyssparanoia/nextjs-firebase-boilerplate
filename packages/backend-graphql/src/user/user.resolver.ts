023b4dfae2e0866b1c570da3b9747f9d087074f6
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-03-24 01:12:45 +0900

    feat: added mutaion create user

--- packages/backend-graphql/src/user/user.resolver.ts
+++ packages/backend-graphql/src/user/user.resolver.ts
@@ -1,5 +1,6 @@
-import { Resolver, Query } from '@nestjs/graphql'
+import { Resolver, Query, Args, Mutation } from '@nestjs/graphql'
 import { User } from './user.entity'
+import { UserCreateInput } from './dto/user.input'
 
 const users = [
   {
@@ -17,5 +18,10 @@ export class UserResolver {
   @Query(() => [User])
   async users(): Promise<User[]> {
     return users
+  }
+
+  @Mutation(() => User)
+  async createUser(@Args('param') param: UserCreateInput): Promise<User> {
+    return { id: 'aaaa', name: param.name }
   }
 }
