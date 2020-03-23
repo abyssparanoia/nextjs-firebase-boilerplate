c122ddc53c153b71910d24b8d578fd14cd2f5361
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-03-24 00:43:52 +0900

    fix: format

--- packages/backend-graphql/src/user/user.resolver.spec.ts
+++ packages/backend-graphql/src/user/user.resolver.spec.ts
@@ -1,18 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UserResolver } from './user.resolver';
+import { Test, TestingModule } from '@nestjs/testing'
+import { UserResolver } from './user.resolver'
 
 describe('UserResolver', () => {
-  let resolver: UserResolver;
+  let resolver: UserResolver
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserResolver],
-    }).compile();
+      providers: [UserResolver]
+    }).compile()
 
-    resolver = module.get<UserResolver>(UserResolver);
-  });
+    resolver = module.get<UserResolver>(UserResolver)
+  })
 
   it('should be defined', () => {
-    expect(resolver).toBeDefined();
-  });
-});
+    expect(resolver).toBeDefined()
+  })
+})
