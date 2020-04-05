4f70dcf39169d0a33676f4f432d27caff6f48981
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-04-05 13:20:22 +0900

    feat: impl use sign out

--- packages/web-graphql/src/fixtures/auth/hooks.ts
+++ packages/web-graphql/src/fixtures/auth/hooks.ts
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { auth, firebase } from 'src/firebase/client'
-import { setTokenToCookie } from './cookie'
+import { setTokenToCookie, removeTokenFromCookie } from './cookie'
 
 export const useSignIn = () => {
   const [error, setError] = useState<Error | undefined>(undefined)
@@ -9,6 +9,7 @@ export const useSignIn = () => {
   const handleSignInWithGoogle = async () => {
     if (isLoading) return
     setIsLoading(true)
+    setError(undefined)
     try {
       const userCredential = await auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).catch(err => {
         throw err
@@ -28,4 +29,27 @@ export const useSignIn = () => {
   }
 
   return { handleSignInWithGoogle, isLoading, error }
+}
+
+export const useSignOut = () => {
+  const [error, setError] = useState<Error | undefined>(undefined)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const handleSignOut = () => {
+    if (isLoading) return
+    setError(undefined)
+    setIsLoading(true)
+    return auth
+      .signOut()
+      .then(() => {
+        removeTokenFromCookie()
+        setIsLoading(false)
+      })
+      .catch(err => {
+        setIsLoading(false)
+        setError(err)
+      })
+  }
+
+  return { handleSignOut, isLoading, error }
 }
