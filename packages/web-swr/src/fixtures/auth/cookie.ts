72c48406088125ecafb8110362f331f89e915d53
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-05-25 14:50:32 +0900

    refactor: rename

---
+++ packages/web-swr/src/fixtures/auth/cookie.ts
@@ -0,0 +1,31 @@
+import { NextPageContext } from 'next'
+import { parseCookies, setCookie, destroyCookie } from 'nookies'
+
+type Credential = {
+  idToken: string
+  refreshToken: string
+}
+
+export const getTokenFromCookie = (ctx?: NextPageContext): Credential | undefined => {
+  const { idToken, refreshToken } = (parseCookies(ctx) as unknown) as Partial<Credential>
+  if (idToken && refreshToken) {
+    return { idToken, refreshToken }
+  }
+  return undefined
+}
+
+export const setTokenToCookie = (cred: Credential, ctx?: NextPageContext) => {
+  setCookie(ctx, 'idToken', cred.idToken, {
+    maxAge: 30 * 24 * 60 * 60,
+    path: '/'
+  })
+  setCookie(ctx, 'refreshToken', cred.refreshToken, {
+    maxAge: 30 * 24 * 60 * 60,
+    path: '/'
+  })
+}
+
+export const removeTokenFromCookie = (ctx?: NextPageContext) => {
+  destroyCookie(ctx, 'idToken')
+  destroyCookie(ctx, 'refreshToken')
+}
